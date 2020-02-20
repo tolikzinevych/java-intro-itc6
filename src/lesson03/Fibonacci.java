992ddffe7488cf4899b58e9ca37f057fd8d41774
Anatolii.Zinevych <anatoliy.zinevych@gmail.com>
2020-02-20 16:25:37 +0200

    fixed issues 1

--- src/lesson03/Fibonacci.java
+++ src/lesson03/Fibonacci.java
@@ -1,12 +1,41 @@
-/*package lesson03;
+package lesson03;
 
 public class Fibonacci {
-    public void printFibonacciNumbers(int n){
-      int a = 0;
-      int b = 0;
-      for (int i=0; i<n; i++){
-//a=b;
-//b=a+b (last iteration)
-      }
+    public int n;
 
-}*/
+    public Fibonacci(int n) {
+        this.n = n;
+    }
+    public String printFibonacciNumbers(){
+        if (n < 0)
+            System.out.println("Please enter a positive number more than 0");
+        if (n == 0)
+            System.out.println("Please enter a positive number more than 0");
+     int a = 0;
+     int b = 0;
+     String s = "";
+
+        for(int i=0; i < n; i++) {
+            b = a + b;
+            s = s + Integer.toString(b) + ", ";
+            a++;
+        }
+        return s;
+    }
+        public static void main(String[] args) {
+            Fibonacci Arr = new Fibonacci(0);
+            String s = Arr.printFibonacciNumbers();
+            System.out.println(s);
+
+        }
+    }
+
+/*public void printFibonacciNumbers(int n) {
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < n; i++) {
+        a = b;
+        b = a + b;
+            System.out.println(b);
+        }
+    }*/
