912689b97c97d7d4fd8bd3b4544a3a7bf277454c
Anatolii.Zinevych <anatoliy.zinevych@gmail.com>
2020-02-20 16:46:52 +0200

    fixed issues 1

--- src/lesson03/Fibonacci.java
+++ src/lesson03/Fibonacci.java
@@ -12,20 +12,22 @@ public class Fibonacci {
         if (n == 0)
             System.out.println("Please enter a positive number more than 0");
      int a = 0;
-     int b = 0;
+     int b = 1;
+     int c;
      String s = "";
 
-        for(int i=0; i < n; i++) {
-            b = a + b;
-            s = s + Integer.toString(b) + ", ";
-            a++;
+        for(int i=0; i < (n-1); i++) {
+            c = a + b;
+            s = s + b + ", ";
+            a = b;
+            b = c;
         }
         return s;
     }
         public static void main(String[] args) {
-            Fibonacci Arr = new Fibonacci(0);
+            Fibonacci Arr = new Fibonacci(4);
             String s = Arr.printFibonacciNumbers();
-            System.out.println(s);
+            System.out.println("0, " + s);
 
         }
     }
