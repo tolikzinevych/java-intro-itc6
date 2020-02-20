b7b2c7647a5b4e705cd92e64025665ae448ee0c3
Anatolii.Zinevych <anatoliy.zinevych@gmail.com>
2020-02-20 17:13:45 +0200

    fixed issues 1

--- src/lesson03/Fibonacci.java
+++ src/lesson03/Fibonacci.java
@@ -32,12 +32,3 @@ public class Fibonacci {
         }
     }
 
-/*public void printFibonacciNumbers(int n) {
-        int a = 0;
-        int b = 0;
-        for (int i = 0; i < n; i++) {
-        a = b;
-        b = a + b;
-            System.out.println(b);
-        }
-    }*/
