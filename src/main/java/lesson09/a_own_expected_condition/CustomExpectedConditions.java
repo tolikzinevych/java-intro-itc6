30f5ec92cc5f9f0a7fd9e9f26fbc1ae1ef341e7c
ANATOLII ZINEVYCH <anatoliy.zinevych@gmail.com>
2020-02-28 13:59:41 +0200

    1

---
+++ src/main/java/lesson09/a_own_expected_condition/CustomExpectedConditions.java
@@ -0,0 +1,9 @@
+package lesson09.a_own_expected_condition;
+
+
+public class CustomExpectedConditions {
+    public static ExpectedCondition<WebElement> listNthElementHasText(
+            By locator, int a, String expTextPart){
+        return null;
+    }
+}
