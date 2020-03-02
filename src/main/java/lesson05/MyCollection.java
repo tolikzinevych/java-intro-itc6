e2d421711957a28c54ffb9b92f322cbb7368d673
Zinevych Anatolii <anatoliy.z@randrmusic.com>
2020-03-02 21:58:40 +0200

    dfsd1

--- src/main/java/lesson05/MyCollection.java
+++ src/main/java/lesson05/MyCollection.java
@@ -7,7 +7,8 @@ import java.util.Iterator;
 
 public class MyCollection {
     public static void main(String[] args) {
-        ArrayList<String>bands = new ArrayList<String>();
+        ArrayList<String> bands = new ArrayList<String>(6);
+        ArrayList<String> bands1 = new ArrayList<String>(6);
         bands.add("Iron Maiden");
         bands.add("Manowar");
         bands.add("Helloween");
@@ -17,10 +18,22 @@ public class MyCollection {
 //        System.out.println(bands);
         bands.remove("Manowar");
         System.out.println(bands);
+        Collections.copy(bands, bands1);
+
         Collections.sort(bands);
         System.out.println(bands);
-        Iterator iterator = bands.iterator();
-        System.out.println(iterator);
-    }
 
+        Iterator iterator = bands.iterator();
+        while (iterator.hasNext()) {
+            String obj = (String) iterator.next();
+            System.out.println(obj);
+        }
+        Iterator iterator1 = bands1.iterator();
+        while (iterator1.hasNext()) {
+            String obj1 = (String) iterator1.next();
+            System.out.println(obj1);
+        }
+    }
 }
+
+
