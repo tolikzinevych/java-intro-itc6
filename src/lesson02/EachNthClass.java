8a85683a7ec272397ec753240016fe67244f63a2
Anatolii.Zinevych <anatoliy.zinevych@gmail.com>
2020-03-02 11:48:47 +0200

    fixed issues 1

--- src/lesson02/EachNthClass.java
+++ src/lesson02/EachNthClass.java
@@ -1,26 +1,30 @@
 package lesson02;
 
 public class EachNthClass {
-    private String text;
-    private int n;
+/*    private String text;
+    private int n;*/
 
-    public EachNthClass(String text, int n) {
-        this.text = text;
-        this.n = n;
+/*
+    public EachNthClass() {
+*/
+/*        this.text = text;
+        this.n = n;*//*
+
     }
+*/
 
-    public String everyNth(String text, int n) {
-        if (text.isEmpty()) {
+    public String everyNth(String text1, int n1) {
+        if (text1.isEmpty()) {
             return "Text shouldn't be empty";
         }
         String s = "";
-        for(int i=0; i < text.length(); i+=n) {
-            s = s + text.charAt(i);
+        for(int i=0; i < text1.length(); i+=n1) {
+            s = s + text1.charAt(i);
         }
         return s;
     }
     public static void main(String[] args) {
-        EachNthClass enc = new EachNthClass("abcdefgh", 2);
+        EachNthClass enc = new EachNthClass();
         String s = enc.everyNth("abcdefg", 2);
         System.out.println(s);
 
